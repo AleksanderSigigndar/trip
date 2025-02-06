08ccd5e5143b3d6e2538e736e92bd1f2eb047aac
AleksanderSigigndar <alexcrasse@gmail.com>
2025-02-06 10:13:46 +0300

    Slash

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Luxury from './component/luxury/Luxury';
+import Luxury from './pages/luxuryPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
