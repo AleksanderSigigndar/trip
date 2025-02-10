904c3f03fc851b1d5498f1e2923a4aa3477fa024
AleksanderSigigndar <alexcrasse@gmail.com>
2025-02-10 12:01:48 +0300

    lll

--- src/pages/luxuryPage/luxurySearch/luxurySearch.jsx
+++ src/pages/luxuryPage/luxurySearch/luxurySearch.jsx
@@ -5,7 +5,32 @@ const Search = () => {
   return (
     <div className={s.Search}>
       <div className={s.Title}>Luxury packages</div>
-      <div className={s.Main}></div>
+      <div className={s.Main}>
+        <div className={s.Info}>
+          <div className={s.Tours}>
+            <div className={s.Name}>Luxury Tours</div>
+            <div className={s.List}>Romantic Winter Destinations</div>
+          </div>
+          <div className={s.When}>
+            <div className={s.Name}>When</div>
+            <div className={s.List}>14 Dec 2022 - 28 Dec 2022</div>
+          </div>
+          <div className={s.Type}>
+            <div className={s.Name}>Travellers</div>
+            <div className={s.List}>2 persons</div>
+          </div>
+          <div className={s.Pick}>
+            <div className={s.pickChoice}>
+              <div className=""></div>
+              <div className=""></div>
+            </div>
+            <div className={s.pickChoice}>
+              <div className=""></div>
+              <div className=""></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
